0e7cfd210a02a1e84dea44f9c1c3de88dfacdb80
renan-b-eth <renanacademic21@gmail.com>
2024-09-30 20:48:13 -0300

    feito

--- app/src/main/java/com/example/projetoodonto/MainActivity.kt
+++ app/src/main/java/com/example/projetoodonto/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.projetoodonto
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tela1)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
+
         }
+        val intent = Intent(this, Tela2::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/projetoodonto/Tela2.kt
+++ app/src/main/java/com/example/projetoodonto/Tela2.kt
@@ -1,6 +1,8 @@
 package com.example.projetoodonto
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,16 @@ class Tela2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tela2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tela1)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+        }
+            val button = findViewById<Button>(R.id.btnLogar)
+        button.setOnClickListener {
+            val intent = Intent(this, GerenciamentoEmpresa::class.java)
+            startActivity(intent)
         }
     }
 }
